9f8709f52602805b83097eefc3225611ce99d68c
Beshoy Fady <beshoyfadykamel1@gmil.com>
2026-03-08 17:30:15 +0200

    feat: add rate limiting on profile routes

--- routes/Api/api.php
+++ routes/Api/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+
+    Route::prefix('profile')->middleware('throttle:60,1')->group(function () {
+        Route::get('/', [ProfileController::class, 'index']);
+        Route::post('/update', [ProfileController::class, 'update']);
+    });
+});
