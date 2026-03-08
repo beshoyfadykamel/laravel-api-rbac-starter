a8f9a12c70b2dd680d9ab02514b8d0e8a013127b
Beshoy Fady <beshoyfadykamel1@gmil.com>
2026-03-08 17:57:00 +0200

    refactor: unify user resource namespace

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Auth\RegisterRequest;
-use App\Http\Resources\Users\UserResource;
+use App\Http\Resources\User\UserResource;
 use App\Models\User;
 use App\Traits\Api\ApiResponse;
 use Illuminate\Support\Facades\Hash;
